5ae72946b8d42790ae7e85719d15bb68994fe6c3
Bartek <kamin.bart@gmail.com>
2021-03-10 15:08:00 +0100

    Repair for Docker

--- Wayo-API/Startup.cs
+++ Wayo-API/Startup.cs
@@ -45,9 +45,10 @@ namespace Wayo_API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Wayo_API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Wayo_API v1"));
 
             app.UseHttpsRedirection();
 
